2994fd8706e076dad3ca0ce79b1e76166fe04d22
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-03-17 21:49:41 +0000

    Solution

---
+++ asset-creator/projects/asset-creator/contracts/AssetCreator.algo.ts
@@ -0,0 +1,46 @@
+import { Contract } from '@algorandfoundation/tealscript';
+
+export class AssetCreator extends Contract {
+  asset = GlobalStateKey<AssetID>();
+
+  createApplication(): void {}
+
+  /**
+   * This creates the asset that can be claimed
+   * once by any address. 
+   */
+  createAsset() {
+    assert(this.txn.sender === this.app.creator);
+    assert(!this.asset.exists);
+
+    const newAsset = sendAssetCreation({
+      configAssetName: 'ASC',
+      configAssetTotal: 10000,
+      configAssetDecimals: 0,
+      configAssetFreeze: this.app.address,
+      configAssetManager: this.app.address,
+    });
+
+    this.asset.value = newAsset;
+  }
+
+  /**
+   * This method transfers and freezes one unit of the
+   * asset to the account that calls it.
+   */
+  claimAsset() {
+    assert(this.txn.sender.assetBalance(this.asset.value) === 0);
+
+    sendAssetTransfer({
+      assetAmount: 1,
+      assetReceiver: this.txn.sender,
+      xferAsset: this.asset.value,
+    });
+
+    sendAssetFreeze({
+      freezeAsset: this.asset.value,
+      freezeAssetFrozen: true,
+      freezeAssetAccount: this.txn.sender,
+    });
+  }
+}
---
+++ client/src/index.ts
@@ -0,0 +1,105 @@
+import * as algokit from "@algorandfoundation/algokit-utils";
+import { AppClient } from "@algorandfoundation/algokit-utils/types/app-client";
+import algosdk, { makeAssetTransferTxnWithSuggestedParamsFromObject, makePaymentTxnWithSuggestedParamsFromObject } from 'algosdk';
+import { ASSET_CREATOR_ARC_32 } from "./client";
+import { MNEMONIC } from "./constant";
+
+async function loadAccount() {
+    const client = algokit.AlgorandClient.fromConfig({
+        algodConfig: {
+            server: "https://testnet-api.algonode.cloud",
+        },
+        indexerConfig: {
+            server: 'https://testnet-idx.algonode.cloud',
+        }
+    });
+
+    const account = client.account.fromMnemonic(MNEMONIC);
+    return { account, client };
+}
+
+async function main() {
+    const { account, client } = await loadAccount();
+
+    // Create factory
+    // const appFactory = client.client.getAppFactory({
+    //     appSpec: JSON.stringify(ASSET_CREATOR_ARC_32),
+    //     defaultSender: account.addr,
+    //     defaultSigner: account.signer,
+    // });
+
+    // const response = await appFactory.send.create({
+    //     method: 'createApplication',
+    // });
+    // const appId = response.result.appId;
+
+    // Load app client for interacting with app
+    const appClient = new AppClient({
+        appId: BigInt(735919053),
+        appSpec: JSON.stringify(ASSET_CREATOR_ARC_32),
+        algorand: client,
+    });
+
+    const suggestedParams = await client.client.algod.getTransactionParams().do();
+
+    // const fundingTxn = makePaymentTxnWithSuggestedParamsFromObject({
+    //     amount: 200_000,
+    //     from: account.addr,
+    //     to: response.result.appAddress,
+    //     suggestedParams,
+    // })
+
+    const atomTransactionComposer = new algosdk.AtomicTransactionComposer();
+
+    // atomTransactionComposer.addTransaction({
+    //     txn: fundingTxn,
+    //     signer: account.signer,
+    // });
+
+    // atomTransactionComposer.addMethodCall({
+    //     method: appClient.getABIMethod('createAsset')!,
+    //     methodArgs: [],
+    //     suggestedParams: {
+    //         ...suggestedParams,
+    //         fee: 2_000,
+    //     },
+    //     sender: account.addr,
+    //     signer: account.signer,
+    //     appID: Number(735919053),
+    // });
+
+    const globalState = await appClient.getGlobalState();
+    const assetId = globalState.asset.value;
+    console.log(globalState.asset.value)
+
+    const assetOptinTxn = makeAssetTransferTxnWithSuggestedParamsFromObject({
+        amount: 0,
+        from: account.addr,
+        to: account.addr,
+        assetIndex: Number(assetId),
+        suggestedParams,
+    });
+
+    atomTransactionComposer.addTransaction({
+        txn: assetOptinTxn,
+        signer: account.signer,
+    });
+
+    atomTransactionComposer.addMethodCall({
+        method: appClient.getABIMethod('claimAsset')!,
+        methodArgs: [],
+        suggestedParams: {
+            ...suggestedParams,
+            fee: 3_000,
+        },
+        sender: account.addr,
+        signer: account.signer,
+        appID: Number(735919053),
+        appForeignAssets: [Number(assetId)],
+    });
+
+    const finalResponse = await atomTransactionComposer.execute(client.client.algod, 8);
+    console.log(finalResponse);
+}
+
+main();
