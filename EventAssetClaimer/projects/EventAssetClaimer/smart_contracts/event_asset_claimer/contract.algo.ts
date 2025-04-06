53cc55aeab15fb76cd529eb3f6ec3e1d4009efdf
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-04-06 16:07:10 +0000

    Create event asset claimer

---
+++ EventAssetClaimer/projects/EventAssetClaimer/smart_contracts/event_asset_claimer/contract.algo.ts
@@ -0,0 +1,56 @@
+import { 
+  Contract, 
+  abimethod, 
+  assert, 
+  gtxn, 
+  uint64, 
+  Global,
+  Asset,
+  Txn,
+  itxn,
+} from '@algorandfoundation/algorand-typescript'
+
+export class EventAssetClaimer extends Contract {
+  @abimethod({ onCreate: 'allow' })
+  public createApplication() {}
+
+  @abimethod()
+  public bootstrap(
+    mbrTxn: gtxn.PaymentTxn, 
+    ipfsUrl: string, 
+    numberOfUnits: uint64, 
+    assetName: string, 
+    assetUnitName: string,
+  ): uint64 {
+    assert(mbrTxn.amount >= 200_000, 'Minimum balance must be at least 0.2 algos');
+    assert(mbrTxn.receiver === Global.currentApplicationAddress);
+    assert(Txn.sender === Global.creatorAddress);
+    assert(numberOfUnits > 0);
+
+    const response = itxn.assetConfig({
+      assetName,
+      unitName: assetUnitName,
+      url: ipfsUrl,
+      decimals: 0,
+      total: numberOfUnits,
+      manager: Global.currentApplicationAddress,
+    }).submit();
+
+    return response.createdAsset.id
+  }
+
+  @abimethod()
+  public claimAsset(assetId: uint64): void {
+    const assetBalance = Asset(assetId).balance(Global.creatorAddress);
+    const userAssetBalance = Asset(assetId).balance(Txn.sender);
+    assert(assetBalance > 0);
+    assert(userAssetBalance === 0);
+    assert(Txn.sender.isOptedIn(Asset(assetId)));
+
+    itxn.assetTransfer({
+      xferAsset: assetId,
+      assetAmount: 1,
+      assetReceiver: Txn.sender,
+    }).submit();
+  }
+}
